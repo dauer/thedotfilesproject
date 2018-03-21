ec6c9c458481f642f86570f2e410230db9349aff
Thomas Rasmussen <thomas.bregentved@gmail.com>
2018-03-21 23:29:55 +0100

    Added .gitconfig template

---
+++ templates/git.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+. ./settings/git.properties
+
+cat << EOF
+[user]
+    email = $EMAIL
+    name = $NAME
+
+[color]
+    ui = true
+    pager = true
+    diff = true
+    status = true
+    cherry = true
+    branch = true
+
+[color "branch"]
+    current = yellow reverse
+    local = yellow
+    remote = green
+
+[color "diff"]
+    meta = cyan
+    frag = magenta
+    old = red
+    new = green
+
+[color "status"]
+    added = yellow
+    changed = green
+    untracked = cyan
+    branch = yellow
+
+[color "interactive"]
+    prompt = yellow
+
+[alias]
+    lg = log --decorate
+
+[core]
+    pager = more
+    editor = vim
+
+[i18n]
+    logOutputEncoding = utf8
+
+[help]
+    autocorrect = 0
+
+[push]
+    default = simple
+
+[pull]
+    rebase = true
+
+[rebase]
+    autoStash = true
+EOF
